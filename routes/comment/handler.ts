8abfd9b3bb0bfd42c5e8643bebd8c9379a9606e5
Byron Dunkley <byronleigh80@outlook.com>
2024-11-30 18:34:34 +0000

    add put and delete comment route

--- routes/comment/handler.ts
+++ routes/comment/handler.ts
@@ -1,6 +1,6 @@
 import { eq, and } from "drizzle-orm";
 import { db } from "@db/db";
-import { selectCommentsSchema, comments } from "@db/schema";
+import { selectCommentsSchema, comments, tasks } from "@db/schema";
 import type { CreateCommentBody } from "@db/schema";
 import type { Context } from "hono";
 
@@ -31,4 +31,64 @@ export async function createCommentHandler(c: CustomContext) {
   };
 
   return c.json(res, 201);
+}
+
+export async function getCommentHandler(c: Context) {
+  const { taskId } = c.req.param();
+  const commentList = await db
+    .select()
+    .from(comments)
+    .where(eq(comments.taskId, taskId));
+
+  if (!commentList) {
+    return c.json(
+      {
+        error: "Comment(s) not found",
+      },
+      404,
+    );
+  }
+
+  return c.json(
+    {
+      data: commentList.map((comment) => selectCommentsSchema.parse(comment)),
+    },
+    200,
+  );
+}
+
+export async function updateCommentHandler(c: CustomContext) {
+  const { id: slug } = c.req.param();
+  const { id: authorId } = c.get("user");
+  const { comment } = c.req.valid("json");
+
+  const result = await db
+    .update(comments)
+    .set({ comment })
+    .where(and(eq(comments.authorId, authorId), eq(comments.slug, slug)));
+
+  if ((result as any)?.rowCount === 0) {
+    return c.json({ error: "Failed to update task" }, 404);
+  }
+
+  return c.json({ message: "Comment updated successfully" }, 200);
+}
+
+export async function deleteCommentHandler(c: Context) {
+  const { id: slug } = c.req.param();
+  const { id: authorId } = c.get("user");
+
+  const comment = await db
+    .delete(tasks)
+    .where(and(eq(comments.slug, slug), eq(comments.authorId, authorId)))
+    .returning({
+      id: tasks.slug,
+    });
+
+  const res: CommentResponse = {
+    id: comment[0].id,
+    message: "Comment removed",
+  };
+
+  return c.json(res, 200);
 }
--- routes/comment/index.ts
+++ routes/comment/index.ts
@@ -1,9 +1,14 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
-import autheticate from "@middleware/auth";
+import authenticate from "@middleware/auth";
 import { insertCommentsSchema } from "@db/schema";
 import { z } from "@hono/zod-openapi";
-import { createCommentHandler } from "./handler";
+import {
+  createCommentHandler,
+  getCommentHandler,
+  updateCommentHandler,
+  deleteCommentHandler,
+} from "./handler";
 
 const commentRoutes = new Hono();
 
@@ -11,12 +16,38 @@ const taskParamId = z.object({
   taskId: z.string(),
 });
 
+const commentParamId = z.object({
+  id: z.string(),
+});
+
 commentRoutes.post(
   "/:taskId",
-  autheticate,
+  authenticate,
   zValidator("param", taskParamId),
   zValidator("json", insertCommentsSchema),
   createCommentHandler,
+);
+
+commentRoutes.get(
+  "/:taskId",
+  authenticate,
+  zValidator("param", taskParamId),
+  getCommentHandler,
+);
+
+commentRoutes.put(
+  "/:id",
+  authenticate,
+  zValidator("param", commentParamId),
+  zValidator("json", insertCommentsSchema),
+  updateCommentHandler as unknown as any,
+);
+
+commentRoutes.delete(
+  "/:id",
+  authenticate,
+  zValidator("param", commentParamId),
+  deleteCommentHandler,
 );
 
 export default commentRoutes;
