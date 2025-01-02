5e136c4c514806eab0985b22c7a401ff69fa4b89
Byron Dunkley <byronleigh80@outlook.com>
2025-01-02 01:13:46 +0000

    refactor userone/ two logic in test file, add tests for PUT request and up tests for POST requests

--- tests/projects.test.js
+++ tests/projects.test.js
@@ -4,27 +4,29 @@ import { createTestRequest, generateTokenUser } from "./util-test";
 import { PASSWORD, projectsList } from "./data";
 import { expect, test, describe } from "bun:test";
 
-describe("retrive data from users projects", async () => {
-  const userOne = {
-    name: "leigh",
-    password: PASSWORD,
-  };
+const userOne = {
+  name: "leigh",
+  password: PASSWORD,
+};
 
-  const userTwo = {
-    name: "peter",
-    password: PASSWORD,
-  };
+const userTwo = {
+  name: "peter",
+  password: PASSWORD,
+};
+
+const userOneTokenPayload = await generateTokenUser(userOne);
+const userTwoTokenPayload = await generateTokenUser(userTwo);
+
+const userOneHeaders = {
+  Authorization: `Bearer ${userOneTokenPayload.token}`,
+};
+
+const userTwoHeaders = {
+  Authorization: `Bearer ${userTwoTokenPayload.token}`,
+};
+
+describe("retrive data from users projects", () => {
   let projects;
-  const userOneTokenPayload = await generateTokenUser(userOne);
-  const userTwoTokenPayload = await generateTokenUser(userTwo);
-
-  const userOneHeaders = {
-    Authorization: `Bearer ${userOneTokenPayload.token}`,
-  };
-
-  const userTwoHeaders = {
-    Authorization: `Bearer ${userTwoTokenPayload.token}`,
-  };
 
   test("Should list all the projects", async () => {
     const req = createTestRequest("/project", {
@@ -42,32 +44,112 @@ describe("retrive data from users projects", async () => {
     expect(result).toEqual(projectsList.one);
   });
 
-  test("Should get project with :id", async () => {
+  test("Should get project with :id with correct credentials", async () => {
+    const req = createTestRequest(`/project/${projects.data[0].slug}`, {
+      method: "GET",
+      headers: userOneHeaders,
+    });
+
+    const res = await app.fetch(req);
+    const result = await res.json();
+
+    expect(res.status).toBe(200);
+    expect(result.data.name).toEqual(projectsList.one[0]);
+  });
+
+  test("Should not get project with :id with no credentials", async () => {
     const req = createTestRequest(`/project/${projects.data[0].slug}`, {
       method: "GET",
     });
     const res = await app.fetch(req);
     await res.json();
     expect(res.status).toBe(401);
+  });
 
-    const reqTwo = createTestRequest(`/project/${projects.data[0].slug}`, {
-      method: "GET",
-      headers: userOneHeaders,
-    });
-
-    const resTwo = await app.fetch(reqTwo);
-    const result = await resTwo.json();
-
-    expect(resTwo.status).toBe(200);
-    expect(result.data.name).toEqual(projectsList.one[0]);
-
-    const reqThree = createTestRequest(`/project/${projects.data[0].slug}`, {
+  test("Should not get project with :id with wrong credentials", async () => {
+    const req = createTestRequest(`/project/${projects.data[0].slug}`, {
       method: "GET",
       headers: userTwoHeaders,
     });
 
-    const resThree = await app.fetch(reqThree);
-    await resThree.json();
-    expect(resThree.status).toBe(200);
+    const res = await app.fetch(req);
+    await res.json();
+    expect(res.status).toBe(200);
+  });
+});
+
+describe("Create new projects", () => {
+  const name = "Project 4";
+  test("Should not be able to create project with no credentials", async () => {
+    const req = createTestRequest("/project", {
+      method: "POST",
+      body: {
+        name,
+      },
+    });
+
+    const res = await app.fetch(req);
+    await res.json();
+    expect(res.status).toBe(401);
+  });
+
+  test("Should be able to create project with credentials", async () => {
+    const req = createTestRequest("/project", {
+      method: "POST",
+      headers: userOneHeaders,
+      body: {
+        name,
+      },
+    });
+
+    const res = await app.fetch(req);
+    await res.json();
+    expect(res.status).toBe(201);
+  });
+});
+
+describe("Create and update project", async () => {
+  const name = "Project 5";
+  const newName = "Project 5 new";
+  let id;
+
+  test("Should not be able to update with no credentials", async () => {
+    const req = createTestRequest("/project", {
+      method: "POST",
+      headers: userOneHeaders,
+      body: {
+        name,
+      },
+    });
+
+    const res = await app.fetch(req);
+    const result = await res.json();
+    id = result.id;
+
+    const reqTwo = createTestRequest(`/project/${id}`, {
+      method: "PUT",
+      body: {
+        name: newName,
+      },
+    });
+
+    const resTwo = await app.fetch(reqTwo);
+    await resTwo.json();
+    expect(resTwo.status).toBe(401);
+  });
+
+  test("Should be able to update project with correct credentials", async () => {
+    const req = createTestRequest(`/project/${id}`, {
+      method: "PUT",
+      body: {
+        name: newName,
+      },
+      headers: userOneHeaders,
+    });
+
+    const res = await app.fetch(req);
+    const result = await res.json();
+    expect(result.message).toBe("Project updated successfully");
+    expect(res.status).toBe(200);
   });
 });
